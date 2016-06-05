de174147bc5cb03132f10f0d6b0f0b9580670a2d
oldham123 <matthewoldham@outlook.com>
2016-06-05 11:05:07 +0100

    More dialog box work

--- MyApp/JS/Directives/morphFab.js
+++ MyApp/JS/Directives/morphFab.js
@@ -9,51 +9,75 @@ function morphFab() {
     return directive;
 
     function link(scope, element, attrs) {
-        var circle = $("#morph-fab");
-        var container = $("#morph-fab-container");
-        var animated = false;
-        var expandedCircleDiameter = Math.ceil(Math.sqrt( Math.pow(convert("100vw"), 2) + Math.pow(convert("30vh"), 2) ));
-        console.log("YAY DIRECTIVES");
-        var initialCircleDiameter = "10vh";
-        container.css("height", "30vh");
-        container.css("width", "100vw");
-        container.css("position", "relative");
-        container.css("overflow", "hidden");
-        container.css("top", "15vh");
-        container.css("left", "40vw");
-        container.css("z-index", 99999999);
+        var circle = $('#morph-fab');
+        var container = $('#morph-fab-container');
+        var content = $('#morph-fab-content');
+        var contentClip = $('#morph-fab-circular-clip');
+        var header = $('#morph-fab-header');
+        var form = $('#morph-fab-header');
+        var fade = $('#morph-fab-fade');
+        var svg = 'morph-fab-svg';
         
-        circle.css("border-radius","50%");
-        circle.css("background-color","#8BC34A");
-        circle.css("height","200px");
-        circle.css("width","200px");
-        circle.css("position","absolute");
-        circle.css("top","50%"); 
-        circle.css("left", "50%");
-        circle.css("transform","translate(-50%,-50%)");
-
+        var animated = false;
+        
+        var expandedCircleDiameter = 10 + Math.ceil(Math.sqrt( Math.pow(convert("100vw"), 2) + Math.pow(convert("30vh"), 2) ));
+        var initialCircleDiameter = "10vh";
+        
+        function svgEntrance() {
+            var svgAnimation = new Vivus(svg, {duration: 100});
+            velocity($('#' + svg), {
+                "opacity": "1"
+            }, 50);
+            svgAnimation.play();
+        };
+        
+        contentClip.css("height", initialCircleDiameter);
+        contentClip.css("width", initialCircleDiameter);
+        
+        circle.css("height", initialCircleDiameter);
+        circle.css("width", initialCircleDiameter);
+        
         circle.on("click", function() {
             if (!animated) {
-                setTimeout( function() {velocity(circle, {
+                velocity(fade, {
+                "opacity": "1"
+                }, 300);
+                velocity(circle, {
                 "width": expandedCircleDiameter + "px",
                 "height": expandedCircleDiameter + "px",
-                backgroundColor: "#03A9F4"
-                }, 250, [0.4, 0.0, 0.2, 1])}, 100);
-                velocity(container, {
-                "top": [ "0px", "easeInSine" ],
-                "left": [ "0px", "easeOutSine" ]
-                }, 350);
+                "top": "50%",
+                "left": "50%",
+                "opacity": "0"
+                }, 300, [0.4, 0.0, 0.2, 1]);
+                velocity(contentClip, {
+                "width": expandedCircleDiameter + "px",
+                "height": expandedCircleDiameter + "px",
+                "top": "50%",
+                "left": "50%"
+                }, 300, [0.4, 0.0, 0.2, 1]);
                 animated = !animated;
-            } else {
+                setTimeout( function() {
+                    svgEntrance(); }, 300)
+                } else {
+                velocity(fade, {
+                "opacity": "0"
+                }, 300);
                 velocity(circle, {
                 "width": initialCircleDiameter,
                 "height": initialCircleDiameter,
-                backgroundColor: "#8BC34A"
-                }, 250, [0.4, 0.0, 0.2, 1]);
-                velocity(container, {
-                "top": [ "15vh", "easeOutSine" ],
-                "left": [ "40vw", "easeInSine" ]
-                });
+                "top": "90%",
+                "left": "95%",
+                "opacity": "1"
+                }, 300, [0.4, 0.0, 0.2, 1]);
+                velocity(contentClip, {
+                "width": initialCircleDiameter,
+                "height": initialCircleDiameter,
+                "top": "90%",
+                "left": "95%",
+                }, 300, [0.4, 0.0, 0.2, 1]);
+                velocity($('#' + svg), {
+                "opacity": "0"
+                }, 50);
                 animated = !animated;
             }
         })
