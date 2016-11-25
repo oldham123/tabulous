12dfa19cb64c789f52afe4b848ee0d2e884bb175
oldham123 <oldham123@users.noreply.github.com>
2016-11-25 22:46:53 +0000

    More animation changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,20 +6,9 @@ $(function() {
 
   setTimeout(function() {
     $('#abstract').css('visibility', 'visible');
-    var ellipsis = new Vivus('abstract', {type: 'delayed', duration: 500}, function() {
-      //$('#ellipsis .st0').animate({ fill: '#333' }, 800);
-      //$('.st1').animate({ fill: '#727272' }, 800);
-      $('#abstract path').animate({ fill: '#000' }, 800);
+    var ellipsis = new Vivus('abstract', {type: 'delayed', duration: 250}, function() {
+      $('#abstract path').animate({ fill: '#000' }, 400);
     });
-    var totalTime = 2000;
-    var dayNo = 5;
-    var monthLastDay = 30;
-    //var totalTime = (((83*203)-3000) / 30) * dayNo;
-    console.log(ellipsis);
-    var stopTime = (totalTime / monthLastDay) * dayNo;
-    console.log(stopTime);
-    //setTimeout(function() { ellipsis.stop(); }, stopTime);
-
-  }, 1000);
+  }, 200);
   
 });
